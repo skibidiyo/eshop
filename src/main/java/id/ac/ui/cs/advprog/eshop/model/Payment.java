7815defbdc14d1e89c86986375a8b7aa7c2ea144
skibidiyo <diyobanu@gmail.com>
2025-03-07 14:38:55 +0700

    [GREEN] Implement payment model

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -13,15 +13,21 @@ public class Payment {
     Map<String, String> paymentData;
 
     public Payment(String id, String method, Map<String,String> paymentData) {
-
+        this.id = id;
+        this.method = method;
+        this.paymentData = paymentData;
     }
 
     public Payment(String id, String method, Map<String,String> paymentData, String status) {
-
+        this(id,method, paymentData);
+        this.setStatus(status);
     }
 
     public void setStatus(String status) {
-
+        if (!"SUCCESS".equals(status) && !"REJECTED".equals(status)) {
+            throw new IllegalArgumentException("Invalid status: " + status);
+        }
+        this.status = status;
     }
 
 }
